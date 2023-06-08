4a4b25c0397e92d5f1486c33f5eb24f5618dcaf4
wokead <135856756+wokead@users.noreply.github.com>
2023-06-09 00:00:13 +0800

    Update index.js

--- wyy/wyy-app/src/request/index.js
+++ wyy/wyy-app/src/request/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 let service = axios.create({
-    baseURL: 'http://localhost:3000/',
+    baseURL: 'http://8.139.7.208:3000/',
     timeout: 3000
 })
 export default service
